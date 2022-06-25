c7c9bafe7a011c8207d38e75b3345abd44ac27bc
rfasich24 <202410103065@mail.unej.ac.id>
2022-06-25 16:12:17 +0700

    final

--- Project PBO 2/Alat.aspx.cs
+++ Project PBO 2/Alat.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Project_PBO_2
 {
-    public partial class Contact : Page
+    public partial class Alat : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
